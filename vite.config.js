c352a95b42baa78eb45cecbd88f4ddab7775fd02
Martina Ibañez Wulbrandt <martinaiwulbrandt@gmail.com>
2026-01-25 20:29:56 -0300

    fix typo

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: { watch: { usePolling: true } },
-	base: "/portfolio/,",
+	base: "/portfolio/",
 });
