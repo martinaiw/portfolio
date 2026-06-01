0aaaf6a42d3c572cb38b373ce88b70d061dc2bf0
Martina Ibañez Wulbrandt <martinaiwulbrandt@gmail.com>
2026-05-31 23:39:16 -0300

    fix: estilos

--- src/UI/TitleCard.jsx
+++ src/UI/TitleCard.jsx
@@ -1,22 +1,11 @@
 import styles from "./TitleCard.module.css";
 
-function TitleCard({ title, width, fontSize, align = "center", paddingLeft }) {
-    const flexAlign = 
-        align === "left" ? "flex-start" : align === "right" ? "flex-end" : "center";
-
-    const customStyles = { 
-        "--card-width": width, 
-        "--card-font-size": fontSize,
-        "--flex-align": flexAlign, 
-		"--text-align": align,
-		"--padding-left": paddingLeft || "0px"
-    };
-
-    return (
-        <div className={styles.titleCardContainer} style={customStyles}>
-            <h2>{title}</h2>
-        </div>
-    );
+function TitleCard({ title }) {
+	return (
+		<div className={styles.titleCardContainer}>
+			<h2>{title}</h2>
+		</div>
+	);
 }
 
 export default TitleCard;
